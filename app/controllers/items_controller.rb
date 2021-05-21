6db1acf67165f71dd19ad5a61f1aea3cc06ccf44
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-05-21 18:32:31 +0900

    ActiveStorageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,3 +1,9 @@
 class ItemsController < ApplicationController
   def index; end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :category_id, :status_id, :prefecture_id, :ship_from_id, :days_to_ship_id, :selling_price, :image).merge(user_id: current_user.id)
+  end
 end
