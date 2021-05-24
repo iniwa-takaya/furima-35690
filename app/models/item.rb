15432d8ce7a29b0103cc68585952bfd2ff0d2f90
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-05-24 16:12:35 +0900

    商品出品機能の修正（#2）

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,27 +6,23 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :fee_status
   belongs_to :status
-  # ActiveHashのバリデーション
-  with_options numericality: { other_than: 1 } do
-    validates :category_id
-    validates :days_to_ship_id
-    validates :prefecture_id
-    validates :fee_status_id
-    validates :status_id
-  end
+  
   # 他のモデルとのアソシエーション
   belongs_to :user
   has_one_attached :image
-  # 投稿機能のバリデーション
+
+  # バリデーション
   with_options presence: true do
     validates :image
     validates :name
     validates :description
-    validates :category_id
-    validates :status_id
-    validates :prefecture_id
-    validates :fee_status_id
-    validates :days_to_ship_id
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :days_to_ship_id
+      validates :prefecture_id
+      validates :fee_status_id
+      validates :status_id
+    end
     validates :selling_price, numericality: {greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999}, format: { with: /\A[0-9]+\z/ }
   end
 end
