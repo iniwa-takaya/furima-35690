f30b2656d647d739a1b9bf48c9abcceafb662101
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-05-25 16:14:01 +0900

    商品情報更新機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -27,7 +27,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    
+    item = Item.find(params[:id])
+    item.update(item_params)
+    redirect_to item_path(item.id)
   end
 
   private
