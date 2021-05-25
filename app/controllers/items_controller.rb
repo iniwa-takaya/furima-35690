7a581164dcfd598428b2feca143aed6e6c93dc86
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-05-25 17:09:38 +0900

    商品情報編集機能の修正（#1）

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,14 +23,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    @item = Item.find(params[:id])
-    unless current_user == @item.user
-      redirect_to action: :index
-    end
+    find_params
+    check_user
   end
 
   def update
-    @item = Item.find(params[:id])
+    find_params
+    check_user
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -43,5 +42,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :status_id, :prefecture_id, :fee_status_id,
                                  :days_to_ship_id, :selling_price, :image).merge(user_id: current_user.id)
+  end
+
+  def find_params
+    @item = Item.find(params[:id])
+  end
+
+  def check_user
+    unless current_user == @item.user
+      redirect_to action: :index
+    end
   end
 end
